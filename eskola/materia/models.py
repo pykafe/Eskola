5309d450c2b12df6abc7fc34e70e0f29a19effce
ony <dejesushonorio03@gmail.com>
2016-05-02 10:51:25 +0900

    create model

---
+++ eskola/materia/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Materia(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return '%s' % self.name
