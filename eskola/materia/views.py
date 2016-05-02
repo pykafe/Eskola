7779599f869f024eec4d7ab48ce3187136dcc09d
mariano <dedeus_mariano@yahoo.com>
2016-05-02 11:17:15 +0900

    adding views

--- eskola/materia/views.py
+++ eskola/materia/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from serializers import MateriaSerializer
+
+
+class MateriaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Materia.objects.all()
+    serializer_class = MateriaSerializer
